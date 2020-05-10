bacc11411d55dd3d1fa67310552f06d433edf804
Arturo Enriquez <arturoccompany@gmail.com>
2020-05-09 23:24:02 -0700

    Svelte App

---
+++ README.md
@@ -0,0 +1,15 @@
+# Svelte App
+
+Base project to create an application with Svelte.
+
+To use this project, you need to rebuild node modules
+
+```bash
+npm i
+```
+
+## How to use
+
+Run `npm run dev` commmand to run the project in development mode.
+
+Run `npm run build` to compile the project for production.
---
+++ src/containers/App.svelte
@@ -0,0 +1,24 @@
+<style>
+  :global(body) {
+    background-color: #ff3e00;
+    color: white;
+    box-sizing: border-box;
+    font-family: sans-serif;
+  }
+
+  .main {
+    position: fixed;
+    display: flex;
+    height: 100vh;
+    width: 100vw;
+    align-items: center;
+    justify-content: center;
+  }
+  h1 {
+    font-size: 64px;
+  }
+</style>
+
+<div class="main">
+  <h1>Hello Svelte</h1>
+</div>
